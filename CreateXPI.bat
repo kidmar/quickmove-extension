cc07bf06e70410110045a69e1644b9a7bbf1bb0a
kidmar <kidservice@gmail.com>
2022-03-03 16:38:42 +0100

    fix .bat

--- CreateXPI.bat
+++ CreateXPI.bat
@@ -1,15 +1,7 @@
-SET WorkingUnit=E:
-SET OutputPath=%WorkingUnit%\Output
-SET CompiledPath=%OutputPath%\Release
-SET VsUnit=C:
-SET VsPath=%VsUnit%\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE
-SET startPath=%CD%
 SET Path7Z=C:\Program Files\7-Zip\7z.exe
-SET WorkaroundPath=%VsPath%\CommonExtensions\Microsoft\VSI\DisableOutOfProcBuild
-SET SolutionName=OMStill_Truck_Manager
 
 REM Nome file da creare
-SET zipFile=quickmove-1.9.0.xpi
+SET zipFile=quickmove-1.10.0.xpi
 SET fullZipFilePath=%USERPROFILE%\desktop\%zipFile%
 
 del %fullZipFilePath%
